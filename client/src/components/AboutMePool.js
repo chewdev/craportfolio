a90e6f342c56e948e3ea7d45713bd3be1df8a20b
chewdev <itsbewald@gmail.com>
2019-01-08 00:38:10 -0800

    Break out pool into separate component

---
+++ client/src/components/AboutMePool.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function AboutMePool() {
+  return (
+    <div className="about-me-resize-container">
+      <div className="about-me-container">
+        <h2 className="about-me-header">About Me</h2>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp; I'm Chris, a full-stack web developer living
+          in Los Angeles, CA. Having graduated with a degree in Mechanical
+          Engineering, I have always enjoyed solving difficult technical
+          challenges. Designing and building is my passion, making your life
+          easier is my goal.
+          <br />
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;When I'm not busy studying web development,
+          I'm tinkering with Arduino and IoT. Outside of tech, I love sports,
+          especially baseball (Let's Go Dodgers!), and traveling.
+          <br />
+        </p>
+        <p className="about-me-quote">
+          <em>Easy is boring, let's have some fun!</em>
+        </p>
+      </div>
+    </div>
+  );
+}
