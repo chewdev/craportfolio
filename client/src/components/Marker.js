8fa1e92d1264748378b446cf6e4f29fcdd2b16f9
chewdev <itsbewald@gmail.com>
2019-01-13 15:25:50 -0800

    Add Marker component

---
+++ client/src/components/Marker.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Marker() {
+  return (
+    <div className="marker">
+      <div className="marker-body" />
+      <div className="marker-cap" />
+    </div>
+  );
+}
