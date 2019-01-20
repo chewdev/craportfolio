ac9d17f42cfe1be4ca4e7f24e4af659125a39e36
chewdev <itsbewald@gmail.com>
2019-01-19 23:53:24 -0800

    Add Questions component

---
+++ client/src/components/Questions.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Typist from "react-typist";
+import QuestionLI from "./QuestionLI";
+
+export default function Questions({ questionArr, textInput, getAnswer }) {
+  return questionArr.length === 0 && textInput !== "" ? (
+    <Typist avgTypingDelay={10} stdTypingDelay={10} cursor={{ show: false }}>
+      <p>No matching questions</p>
+    </Typist>
+  ) : (
+    <ul>
+      {questionArr.map(obj => (
+        <QuestionLI getAnswer={getAnswer} obj={obj} />
+      ))}
+    </ul>
+  );
+}
