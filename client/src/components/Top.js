483c6cd171b595988f034885e2c1fce01ecf6123
chewdev <itsbewald@gmail.com>
2019-01-23 23:21:57 -0800

    Add Top component to encapsulate top section of page

---
+++ client/src/components/Top.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import Introduction from "./Introduction";
+
+export default function Top() {
+  return (
+    <div id="top-level" className="background-div">
+      <Header />
+      <Introduction />
+    </div>
+  );
+}
