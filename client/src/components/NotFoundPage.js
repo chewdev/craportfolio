f0ffb20a3b6315fd15f91629dc0676c8a8a9c5e7
chewdev <itsbewald@gmail.com>
2019-01-27 00:00:04 -0800

    Fix NotFoundPage to resemble main home page

--- client/src/components/NotFoundPage.js
+++ client/src/components/NotFoundPage.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment } from "react";
+import Top from "./Top/Top";
+import PaperBoxLayout from "./Top/PaperBoxLayout";
+import Footer from "./Footer";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => (
-  <div>
-    404 - <Link to="/">Go home</Link>
-  </div>
+  <Fragment>
+    <Top>
+      <PaperBoxLayout>
+        <div className="notfound">
+          404 - Page Not Found <br />
+          <Link to="/">Go home</Link>
+        </div>
+      </PaperBoxLayout>
+    </Top>
+    <Footer />
+  </Fragment>
 );
 
 export default NotFoundPage;
