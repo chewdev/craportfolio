9dc935a9990993dfe48ab0b4acede983a7f56a69
chewdev <itsbewald@gmail.com>
2019-01-30 01:14:51 -0800

    Use Image, Info, Guide and ProjectRemote components

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -1,5 +1,9 @@
 import React from "react";
 import projectData from "../../data/projects";
+import ProjectRemote from "./ProjectRemote";
+import Guide from "./Guide";
+import Image from "./Image";
+import Info from "./Info";
 
 class Projects extends React.Component {
   constructor(props) {
@@ -158,184 +162,59 @@ class Projects extends React.Component {
   };
 
   render() {
-    const project = projectData[this.state.currProjInd];
-    const liArr = (() => {
-      let liArr = [];
-      for (let i = 0; i <= 9; i++) {
-        liArr.push(
-          <li
-            className="projects-remote-input"
-            key={`remote-${i}`}
-            onClick={() => {
-              this.remoteSetInd(i);
-            }}
-          >
-            <button className="projects-remote-input-div">{i}</button>
-          </li>
-        );
-      }
-      return liArr;
-    })();
-
-    if (this.state.tvDisplayType === "guide") {
-      var projArr = projectData.map((project, ind) => {
-        return (
-          <li
-            className="projects-tv-list-item"
-            key={ind}
-            onClick={() => {
-              this.remoteSetInd(ind);
-              this.guide();
-            }}
-          >
-            <span className="channel-name-span">{project.channelName}</span>
-            <span className="channel-ind-span">{ind}</span>
-            <span
-              className={
-                "channel-title-span" +
-                (this.state.currProjInd === ind
-                  ? " projects-tv-list-item--selected"
-                  : "")
-              }
-            >
-              {project.title}
-            </span>
-          </li>
-        );
-      });
-    }
+    const {
+      onOpenProject,
+      remoteSetInd,
+      guide,
+      changeAuto,
+      info,
+      up,
+      down,
+      nextProj,
+      ok,
+      prevProj,
+      state
+    } = this;
+    const {
+      currProjInd,
+      tvDisplayType,
+      remoteLightClass,
+      projIntervalId
+    } = state;
+    const project = projectData[currProjInd];
 
     return (
       <div className="project-tv-remote-container">
         <div className="project-container">
-          {this.state.tvDisplayType === "image" ? (
-            <a
-              className="project-link"
-              href={project.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              onClick={this.onOpenProject}
-            >
-              <img
-                className="project-tv"
-                src={project.imgSrc}
-                alt={project.alt}
-              />
-            </a>
-          ) : this.state.tvDisplayType === "info" ? (
-            <div className="project-info project-link">
-              <h2>{project.title}</h2>
-              <p>{project.description}</p>
-            </div>
+          {tvDisplayType === "image" ? (
+            <Image project={project} onOpenProject={onOpenProject} />
+          ) : tvDisplayType === "info" ? (
+            <Info project={project} />
           ) : (
-            <div className="project-link">
-              <ul>{projArr}</ul>
-            </div>
+            <Guide
+              projectData={projectData}
+              remoteSetInd={remoteSetInd}
+              guide={guide}
+              currProjInd={currProjInd}
+            />
           )}
           <div className="top-tv-base" />
           <div className="bottom-tv-base" />
         </div>
 
-        <ul className="projects-remote">
-          <li className="projects-remote-number-li projects-remote-number-li--container">
-            <ul className="projects-remote-number-ul projects-remote-number-ul--power">
-              <li className="projects-remote-input" onClick={this.changeAuto}>
-                <button className="projects-remote-input-div">
-                  {this.state.projIntervalId === null ? (
-                    <i className="fas fa-play" />
-                  ) : (
-                    <i className="fas fa-pause" />
-                  )}
-                </button>
-              </li>
-              <li className="projects-remote-input projects-remote-input--power">
-                <div
-                  className={`projects-remote-power-ind ${
-                    this.state.remoteLightClass
-                  }`}
-                />
-              </li>
-            </ul>
-          </li>
-          <li className="projects-remote-number-li projects-remote-number-li--container">
-            <ul className="projects-remote-number-ul projects-remote-number-ul--arrows">
-              <li
-                className="projects-remote-input projects-remote-input--guide"
-                onClick={this.guide}
-              >
-                <button className="projects-remote-input-div projects-remote-input-div--text">
-                  {" "}
-                  GUIDE{" "}
-                </button>
-              </li>
-              <li
-                className="projects-remote-input projects-remote-input--info"
-                onClick={this.info}
-              >
-                <button className="projects-remote-input-div projects-remote-input-div--text">
-                  {" "}
-                  INFO{" "}
-                </button>
-              </li>
-            </ul>
-          </li>
-          <li className="projects-remote-number-li projects-remote-number-li--container">
-            <ul className="projects-remote-number-ul projects-remote-number-ul--arrows">
-              <li className="projects-remote-input" onClick={this.up}>
-                <button className="projects-remote-input-div">
-                  {" "}
-                  <i className="fas fa-chevron-up" />{" "}
-                </button>
-              </li>
-            </ul>
-          </li>
-          <li className="projects-remote-number-li projects-remote-number-li--container">
-            <ul className="projects-remote-number-ul projects-remote-number-ul--arrows">
-              <li className="projects-remote-input" onClick={this.prevProj}>
-                <button className="projects-remote-input-div">
-                  {" "}
-                  <i className="fas fa-chevron-left" />{" "}
-                </button>
-              </li>
-              <li
-                className="projects-remote-input projects-remote-input--ok"
-                onClick={this.ok}
-              >
-                <button className="projects-remote-input-div projects-remote-input-div--ok">
-                  OK
-                </button>
-              </li>
-              <li className="projects-remote-input" onClick={this.nextProj}>
-                <button className="projects-remote-input-div">
-                  {" "}
-                  <i className="fas fa-chevron-right" />{" "}
-                </button>
-              </li>
-            </ul>
-          </li>
-          <li className="projects-remote-number-li projects-remote-number-li--container">
-            <ul className="projects-remote-number-ul projects-remote-number-ul--arrows">
-              <li className="projects-remote-input" onClick={this.down}>
-                <button className="projects-remote-input-div">
-                  {" "}
-                  <i className="fas fa-chevron-down" />{" "}
-                </button>
-              </li>
-            </ul>
-          </li>
-          <li className="projects-remote-number-li">
-            <ul className="projects-remote-number-ul">{liArr.slice(1, 4)}</ul>
-          </li>
-          <li className="projects-remote-number-li">
-            <ul className="projects-remote-number-ul">{liArr.slice(4, 7)}</ul>
-          </li>
-          <li className="projects-remote-number-li">
-            <ul className="projects-remote-number-ul">{liArr.slice(7, 10)}</ul>
-          </li>
-          <li>
-            <ul className="projects-remote-number-ul">{liArr.slice(0, 1)}</ul>
-          </li>
-        </ul>
+        <ProjectRemote
+          remoteSetInd={remoteSetInd}
+          changeAuto={changeAuto}
+          remoteLightClass={remoteLightClass}
+          projIntervalId={projIntervalId}
+          guide={guide}
+          info={info}
+          up={up}
+          prevProj={prevProj}
+          down={down}
+          nextProj={nextProj}
+          ok={ok}
+        />
       </div>
     );
   }
