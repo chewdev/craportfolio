6f0b76367498b225ba71de788e3be5401f5af26c
chewdev <itsbewald@gmail.com>
2019-02-03 02:08:47 -0800

    Add prop options for guide, info and ok buttons

--- client/src/components/Projects/RemoteButton.js
+++ client/src/components/Projects/RemoteButton.js
@@ -1,9 +1,23 @@
 import React from "react";
+import classnames from "classnames";
 
-export default function RemoteButton({ onClick, children }) {
+export default function RemoteButton({ onClick, children, guide, ok }) {
   return (
-    <li className="projects-remote-input" onClick={onClick}>
-      <button className="projects-remote-input-div">{children}</button>
+    <li
+      className={classnames("projects-remote-input", {
+        "projects-remote-input--guide": guide,
+        "projects-remote-input--ok": ok
+      })}
+      onClick={onClick}
+    >
+      <button
+        className={classnames("projects-remote-input-div", {
+          "projects-remote-input-div--text": guide,
+          "projects-remote-input-div--ok": ok
+        })}
+      >
+        {children}
+      </button>
     </li>
   );
 }
