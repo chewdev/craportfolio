d60f1b18cd9f7b2373fe7e81ce4c523d173c4cde
chewdev <itsbewald@gmail.com>
2019-02-04 00:02:02 -0800

    Add Table component

---
+++ client/src/components/AboutMe/Table.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Tablet from "./Tablet";
+
+export default function Table() {
+  return (
+    <div className="about-me__table-container">
+      <h2>Skills</h2>
+      <Tablet />
+    </div>
+  );
+}
