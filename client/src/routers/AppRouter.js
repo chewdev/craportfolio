4189672fddfac764752674834092301ff111293c
chewdev <itsbewald@gmail.com>
2019-03-30 23:40:06 -0700

    Update deprecated import for history package.

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -1,10 +1,10 @@
 import React, { Fragment } from "react";
 import { Router, Route, Switch } from "react-router-dom";
-import createHistory from "history/createBrowserHistory";
+import { createBrowserHistory } from "history";
 import DashboardPage from "../components/DashboardPage";
 import NotFoundPage from "../components/NotFoundPage";
 
-export const history = createHistory();
+export const history = createBrowserHistory();
 
 const AppRouter = () => (
   <Router history={history}>
