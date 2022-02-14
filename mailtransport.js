7429d688584ccccac44a9c74ac659adcd3752230
chewdev <itsbewald@gmail.com>
2022-02-14 03:36:26 -0800

    Use googleapis package to retrieve refreshed access tokens when sending mail

--- mailtransport.js
+++ mailtransport.js
@@ -1,21 +1,50 @@
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
 const keys = require("./config/keys");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  service: "gmail",
-  auth: {
-    type: "OAuth2",
-    user: keys.SEND_EMAIL,
-    clientId: keys.GOOGLE_CLIENT_ID,
-    clientSecret: keys.GOOGLE_SECRET,
-    refreshToken: keys.GOOGLE_REFRESH,
-    accessToken: keys.GOOGLE_ACCESS
-  },
-  tls: {
-    rejectUnauthorized: false
-  }
-});
+// Ref: https://dev.to/chandrapantachhetri/sending-emails-securely-using-node-js-nodemailer-smtp-gmail-and-oauth2-g3a
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    keys.GOOGLE_CLIENT_ID,
+    keys.GOOGLE_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: keys.GOOGLE_REFRESH
+  });
+
+  //TODO: handle reject error with catch
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject("Failed to create access token");
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: keys.SEND_EMAIL,
+      accessToken,
+      clientId: keys.GOOGLE_CLIENT_ID,
+      clientSecret: keys.GOOGLE_SECRET,
+      refreshToken: keys.GOOGLE_REFRESH
+    }
+  });
+
+  return transporter;
+}
+
+const sendEmail = async (emailOptions, callback) => {
+  let emailTransporter = await createTransporter();
+  await emailTransporter.sendMail(emailOptions, callback);
+}
 
 const sendMail = ({ email, name, comments, selectOption }, callback) => {
   const mailOptions = {
@@ -26,7 +55,7 @@ const sendMail = ({ email, name, comments, selectOption }, callback) => {
   };
 
   // Try to send email with contact form information
-  transporter.sendMail(mailOptions, callback);
+  sendEmail(mailOptions, callback);
 };
 
-module.exports = { transporter, sendMail };
+module.exports = { sendMail };
